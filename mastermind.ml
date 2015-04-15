ddfa1b847f5e7456f88ec1dec44871a2c871d2a2
V1nc3n7 <vincent.renard45@gmail.com>
2015-04-15 14:19:10 +0200

    elaguage code

--- mastermind.ml
+++ mastermind.ml
@@ -18,54 +18,10 @@ let  occur liste couleur=
     |[] -> n
     |h::t -> if h=couleur then aux t couleur (n+1) else aux t couleur n 
   in aux liste couleur 0;;
-
-(*
-  let rec elag_utile liste=
-  match liste with
-  [] -> false
-  |h::t -> if occur liste h > 1 then true else elag_utile t;;
-  
-  let elagage liste =
-  if elag_utile liste then (elaguerliste liste) else liste ;; *)
 let elaguerliste l = 
   let rec aux l res =
     match l with 
-      []-> res 
-    |h::t -> if (occur res h)> 1 then aux t res else aux res::h
-  in aux l [];;
-type couleur= Rouge | Bleu | Vert | Blanc | Jaune | Noir | Violet | Orange;;
-
-let taille=5;;
-let liste_couleurs =  [ "rouge";"vert";"blanc";"noir";"bleu";"jaune";"violet";"orange"] ;;
-let rec  construire taille =
-  let rec construire_aux liste =
-    match liste with
-    |[]->[]
-    |h::t -> (("rouge"::h)::(("vert"::h)::(("blanc"::h)::(("noir"::h)::(("bleu"::h)::(("jaune"::h)::(("violet"::h)::(("orange"::h)::(construire_aux t)))))))))
-  in match taille with
-  |1 ->[["rouge"];["vert"];["blanc"];["noir"];["bleu"];["jaune"];["violet"];["orange"]]
-  |_->construire_aux (construire(taille-1));;
-
-
-let  occur liste couleur=
-  let rec aux liste couleur n =
-    match liste with 
-    |[] -> n
-    |h::t -> if h=couleur then aux t couleur (n+1) else aux t couleur n 
-  in aux liste couleur 0;;
-
-(*
-  let rec elag_utile liste=
-  match liste with
-  [] -> false
-  |h::t -> if occur liste h > 1 then true else elag_utile t;;
-  
-  let elagage liste =
-  if elag_utile liste then (elaguerliste liste) else liste ;; *)
-let elaguerliste l = 
-  let rec aux l res =
-    match l with 
-      []-> res 
+    |[]-> res 
     |h::t -> if (occur res h)> 1 then aux t res else aux res::h
   in aux l [];;
   
@@ -90,7 +46,7 @@ let game2 x =
 let game3 x =
   let rec aux x=
     match x with
-    |0->failwith "End Game"
+    |0->failwith "End Game" (* on termine le jeu *)
     |1->jouer(construire 5)
     |2->main()
     |_->failwith "Erreur"
